1ae208b7d0616523795dd6c2013f9887c56a5999
suleymancansimsek <suleymancansimsek@gmail.com>
2022-12-28 16:41:25 +0300

    animation added

--- script.js
+++ script.js
@@ -19,7 +19,23 @@ window.addEventListener('load', function(){
     }
 
     class Player {
+        constructor(game){
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0.2;
+        }
 
+        update(){
+            this.y += this.speedY;
+        }
+
+        draw(context){
+            context.fillRect(this.x, this.y, this.width, this.height);
+
+        }
     }
 
     class Enemy {
@@ -39,6 +55,29 @@ window.addEventListener('load', function(){
     }
 
     class Game{
-        
+        constructor(width,height){
+            this.width = width;
+            this.height = height;
+            this.Player = new Player(this);
+        }
+
+        update(){
+            this.Player.update();
+        }
+
+        draw(context){
+            this.Player.draw(context);
+        }
     }
+    const game = new Game(canvas.width, canvas.height);
+
+    //animation loop
+    function animate(params) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.update();
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 });
