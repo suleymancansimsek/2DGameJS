dd444311c416b4c510fc7bcfddbb6204ec769f95
suleymancansimsek <suleymancansimsek@gmail.com>
2022-12-28 23:12:00 +0300

    Drawing Game Score

--- script.js
+++ script.js
@@ -157,16 +157,24 @@ window.addEventListener('load', function(){
             this.game = game;
             this.fontSize = 25;
             this.fontFamily = 'Helvetice';
-            this.color = 'yellow';
+            this.color = 'white';
         }
 
         draw(context){
+            context.save();
+            context.fillStyle = this.color;
+            context.shadowOffsetX = 2;
+            context.shadowOffsetY = 2;
+            context.shadowColor = 'black';
+            context.font = this.fontSize + 'px ' + this.fontFamily;
+            //Score
+            context.fillText('Score: ' + this.game.score, 20, 40);
             //ammo
             context.fillStyle = this.color;
             for (let i = 0; i < this.game.ammo; i++) {
                 context.fillRect(20 + 5 * i, 50, 3, 20);
-                
             }
+            context.restore();
         }
     }
 
@@ -186,6 +194,8 @@ window.addEventListener('load', function(){
             this.ammoTimer = 0;
             this.ammoInterval = 500;
             this.gameOver = false;
+            this.score = 0;
+            this.winningScore = 10;
         }
 
         update(deltaTime){
@@ -209,6 +219,9 @@ window.addEventListener('load', function(){
                         if (enemy.lives <= 0) {
                             enemy.markedForDeletion = true;
                             this.score += enemy.score;
+                            if (this.score > this.winningScore) {
+                                this.gameOver = true;
+                            }
                         }
                     }
                 });
