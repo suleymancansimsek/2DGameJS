f6289d3f1e25573f210f146a8062ccb10697cc87
suleymancansimsek <suleymancansimsek@gmail.com>
2022-12-29 15:28:45 +0300

    Sprite animation for player

--- script.js
+++ script.js
@@ -73,9 +73,13 @@ window.addEventListener('load', function(){
             this.height = 190;
             this.x = 20;
             this.y = 100;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.maxFrame = 37;
             this.speedY = 0.2;
             this.maxSpeed = 5;
             this.projectiles = [];
+            this.image = document.getElementById('player');
         }
 
         update(){
@@ -93,11 +97,18 @@ window.addEventListener('load', function(){
                 projectile.update();
             });
             this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
+            //sprite animation
+            if (this.frameX < this.maxFrame) {
+                this.frameX++;
+            }else {
+                this.frameX = 0;
+            }
         }
 
         draw(context) {
             context.fillStyle = 'black';
             context.fillRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
             this.projectiles.forEach(projectile => {
                 projectile.draw(context);
             });
