0fe486cb6b73b6162b8389d8992802b1d0fdb365
suleymancansimsek <suleymancansimsek@gmail.com>
2022-12-30 17:38:38 +0300

    Drone enemy class added

--- script.js
+++ script.js
@@ -260,7 +260,7 @@ window.addEventListener('load', function(){
             super(game);
             this.width = 213 ;
             this.height = 165 ;
-            this.y = Math.random() * (this.game.height * 0.9 - this.height);
+            this.y = Math.random() * (this.game.height * 0.95 - this.height);
             this.image = document.getElementById('angler2');
             this.frameY = Math.floor(Math.random() * 2);
             this.lives = 3;
@@ -273,12 +273,43 @@ window.addEventListener('load', function(){
             super(game);
             this.width = 99 ;
             this.height = 95 ;
-            this.y = Math.random() * (this.game.height * 0.9 - this.height);
+            this.y = Math.random() * (this.game.height * 0.95 - this.height);
             this.image = document.getElementById('lucky');
             this.frameY = Math.floor(Math.random() * 2);
             this.lives = 3;
             this.score = 15;
             this.type = 'lucky';
+        }
+    }
+
+    class HiveWhale extends Enemy {
+        constructor(game){
+            super(game);
+            this.width = 400 ;
+            this.height = 227 ;
+            this.y = Math.random() * (this.game.height * 0.95 - this.height);
+            this.image = document.getElementById('hivewhale');
+            this.frameY = 0;
+            this.lives = 15;
+            this.score = this.lives;
+            this.type = 'hive';
+            this.speedX = Math.random() * -1.2 - 0.2;
+        }
+    }
+
+    class Drone extends Enemy {
+        constructor(game, x, y){
+            super(game);
+            this.width = 115;
+            this.height = 95;
+            this.x = x;
+            this.y = y;
+            this.image = document.getElementById('drone');
+            this.frameY = Math.floor(Math.random() * 2);
+            this.lives = 3;
+            this.score = this.lives;
+            this.type = 'drone';
+            this.speedX = Math.random() * -4.2 - 0.5;
         }
     }
 
@@ -426,7 +457,7 @@ window.addEventListener('load', function(){
                 enemy.update();
                 if (this.checkCollisions(this.player, enemy)) {
                     enemy.markedForDeletion = true;
-                    for (let i = 0; i < 10; i++) {
+                    for (let i = 0; i < enemy.score; i++) {
                         this.particles.push(new Particle(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height + 0.5));
                     }
                     if (enemy.type === 'lucky') {
@@ -439,10 +470,15 @@ window.addEventListener('load', function(){
                         projectile.markedForDeletion = true;
                         this.particles.push(new Particle(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height + 0.5));
                         if (enemy.lives <= 0) {
-                            for (let i = 0; i < 10; i++) {
+                            for (let i = 0; i < enemy.score; i++) {
                                 this.particles.push(new Particle(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height + 0.5));
                             }
                             enemy.markedForDeletion = true;
+                            if(enemy.type === 'hive'){
+                                for(let i = 0; i < 5; i++ ){
+                                this.enemies.push(new Drone(this,enemy.x + Math.random() * enemy.width, enemy.y + Math.random() * enemy.height * 0.5));
+                            }
+                            }
                             if(!this.gameOver) this.score += enemy.score;
                             if (this.score > this.winningScore) {
                                 this.gameOver = true;
@@ -477,6 +513,8 @@ window.addEventListener('load', function(){
                 this.enemies.push(new Angler1(this));
             }else if(randomize < 0.6){
                 this.enemies.push(new Angler2(this));             
+            }else if(randomize < 0.8){
+                this.enemies.push(new HiveWhale(this));             
             }else{
                 this.enemies.push(new LuckyFish(this));
             }
